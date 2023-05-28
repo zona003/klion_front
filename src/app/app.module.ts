2d9cf0cca09b681c2f7b79dc32ca924f73be2e11
zona003 <zona0031256@gmail.com>
2023-05-29 01:03:23 +0300

    login htm complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes =[
+    {path: 'login', component:LoginComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
