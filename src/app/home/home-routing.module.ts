5227d6fdd9c928fadce76599cbbdf2a0bc41de1d
zona003 <zona0031256@gmail.com>
2023-06-28 23:03:48 +0300

    vacation task done, conf routing

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -7,14 +7,21 @@ import { CardComponent } from '../card/card.component';
 import { ContactsComponent } from '../contacts/contacts.component';
 import { ProfileComponent } from '../profile/profile.component';
 
+import { CardDetailVacationComponent } from '../card_details/card-detail-vacation/card-detail-vacation.component'
+import { CardDetailRandomComponent } from '../card_details/card-detail-random/card-detail-random.component';
+import { CardDetailInvoiceComponent } from '../card_details/card-detail-invoice/card-detail-invoice.component';
+
 const routes: Routes = [
     {
         path: '', component: HomeComponent,
         children: [
-            { path: '', component: CardComponent},
-            { path: 'card', component: CardComponent },
-            { path: 'contacts', component: ContactsComponent },
-            {path : 'profile', component: ProfileComponent}
+            { path: '', component: CardComponent}
+            , { path: 'card', component: CardComponent }
+            , { path: 'contacts', component: ContactsComponent }
+            , { path : 'profile', component: ProfileComponent}
+            , { path : 'detail-vacation', component: CardDetailVacationComponent}
+            , { path : 'detail-random', component: CardDetailRandomComponent}
+            , { path : 'detail-invoice', component: CardDetailInvoiceComponent}
         ]
     }
 ];
