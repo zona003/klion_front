a336e977f519a52e9a496a3c213866e01c827e77
zona003 <zona0031256@gmail.com>
2023-06-30 11:53:13 +0300

    Change detail random/task fix

--- src/app/card_details/card-detail-random/card-detail-random.component.ts
+++ src/app/card_details/card-detail-random/card-detail-random.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/_model/tasks/task';
+import { Router, ActivatedRoute } from '@angular/router';
+import { DetailService } from 'src/app/_service/detail.service';
+import { AccountService } from 'src/app/_service/acount.service';
 
 @Component({
   selector: 'app-card-detail-random',
@@ -6,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-detail-random.component.css']
 })
 export class CardDetailRandomComponent {
+  uid?: string;
+  currentTask? : Task;
 
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private detService: DetailService,
+    private accountService: AccountService
+  ){}
+
+  ngOnInit(): void {
+    this.uid = this.route.snapshot.params['uid'];
+    this.currentTask = this.detService.tasks.find(el => el.Uid === this.uid);
+    if(this.currentTask===undefined)
+    {
+      this.router.navigate(['/home']);
+    }
+  }
+
+  AcceptButtonClick(){
+    if(this.uid !== undefined){
+      this.accountService.updateAnyTaskStatus(this.uid, true);
+    }
+    this.router.navigate(['/home']);
+  }
 }
